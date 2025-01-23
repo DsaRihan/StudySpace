d32d41c9ee7902da46769b65872b743b6ec60c8b
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-23 09:25:43 +0530

    signup functionality

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,7 +1,9 @@
 const User = require('../models/User')
 const OTP = require('../models/OTP');
 const otpgen = require("otp-generator")
+const bcrypt = require("bcrypt")
 
+//sendOTP
 exports.SendOtp = async(req,res)=>{
     try{
         // fetch the emial
@@ -45,9 +47,81 @@ exports.SendOtp = async(req,res)=>{
     }
 }
 
-//sendOTP
-
 //signUp
+exports.signUp = async(req, res) => {
+    try{
+        // fetch the data
+        const {firstName, lastName, email, password, confirmPassword, accountType, contactNumber, otp} = req.body;
+
+        // validate
+        if(!firstName || !lastName || !email || !password || !accountType || !contactNumber || !otp){
+            return res.status(400).json({
+                success:false,
+                message : "All fields are required "
+            })} 
+        
+        // compare the password
+        if(password != confirmPassword){
+            res.status(400).json({
+                success:false,
+                message : "Password mismatch"
+            });
+        }
+
+        // check if the user already exists
+        const checkuser = await User.findOne({ email})
+        if(checkuser){
+            return res.status(400).json({
+                success:false,
+                message : "User already exists"
+            })
+        }
+
+        // find the most recent otp of the user
+        const recentotp = await OTP.find({ email}).sort({createdAt:-1}).limit(1)
+
+        // validate the otp
+        if(recentotp.length == 0){
+            return res.status(400).json({
+                success:false,
+                message : "otp not found"
+            })
+        }else if(otp != recentotp){
+            return res.status(400).json({
+                success:false,
+                message : "invalid otp"
+            })
+        }
+
+        // hash the password
+        const hashedpassword = await bcrypt.hash(password,10)
+
+        // create entry in the database
+        const profiledetails = await Profile.create({
+            gender:null,
+            dateofBirth:null,
+            about:null,
+            contactNumber:null,
+        })
+
+        const user = await User.create({
+            firstName, lastName, email, password:hashedpassword, confirmPassword, accountType,
+            contactNumber, additionalDetails:profiledetails._id,
+            image:`https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`
+        })
+
+        return res.status(200).json({
+            success: true,
+            message:"Successfully registered"
+        })
+    }
+    catch(err){
+        return res.status(400).json({
+            success:false,
+            message:err.message
+        })
+    }
+}
 
 //Login
 
