3ca24294eb9299f8cbc8e0517c60e6f01a3fb53b
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-24 09:05:30 +0530

    authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,41 @@
+const jwt = require('jsonwebtoken');
+require("dotenv").config()
+const User = require("../models/User")
+
 //auth
+exports.getAuth = async (req,res,next)=>{
+    try{
+        // extract the token
+        const token = req.header("Authorization").replace("Bearer ", "");
+
+        // if the token is missing
+        if(!token){
+            return res.status(403).json({
+                success: false,
+                message: "Invalid token"
+            })
+        }
+
+        // verify the token
+        try{
+            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decode;
+        }
+        catch(err){
+            res.status(403).json({
+                success: false,
+                message: "Invalid token"
+            });
+        }
+        next();
+    }
+    catch(e){
+        res.status(403).json({
+            success: false,
+            message: "Something went wrong"
+        });
+    }
+}
 
 //isStudent
 
