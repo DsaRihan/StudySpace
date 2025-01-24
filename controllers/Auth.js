b2df87fddf2f153774d5f5d283f9f92c4b4d4a19
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-24 08:52:11 +0530

    login functionality

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User')
 const OTP = require('../models/OTP');
 const otpgen = require("otp-generator")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 //sendOTP
 exports.SendOtp = async(req,res)=>{
@@ -124,6 +125,65 @@ exports.signUp = async(req, res) => {
 }
 
 //Login
+exports.logIn = async(req,res)=>{
+    try{
+        // extract the data 
+        const {email, password} = req.body;
+
+        // validate
+        if(!email || !password){
+            res.status(403).json({
+                success:false,
+                message:"please fill in both details"
+            })
+        }
+
+        // check if the user exists
+        const user = await User.findOne({email})
+        if(!user){
+            res.status(400).json({
+                success:false,
+                message:"user not found, please sign up"
+            })   
+        }
+
+        // password match and generate JWT
+        if(await bcrypt.compare(password, user.password)) {
+            const payload = {
+                email: user.email,
+                id: user._id,
+                role: user.accountType
+            };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "2h"});
+            user.token = token;
+            user.password = undefined;
+            
+            const options = {
+            expires:new Date(Date.now()+3*24*60*60*1000),
+            httpOnly: true, 
+        }
+        res.cookie("token", token, options).status(200).json({
+        success: true,
+        token,
+        user,
+        message: "Login successful"
+        })
+        }
+        else {
+        return res.status(401).json({
+            success: false,
+            message: "password is incorrect"
+        })
+        }
+    }
+    catch(err){
+        console.log(err)
+        return res.status(401).json({
+        success: false,
+        message: "Login failed"
+        });
+    }
+}
 
 //changePassword
 
