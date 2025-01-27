fda8f1c5ec04847e5c5ec81a319a4effa6235ff1
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-27 20:36:54 +0530

    course creation logic

---
+++ controllers/Course.js
@@ -0,0 +1,52 @@
+const User = require("../models/User")
+const Tag = require("../models/tags")
+const Course = require("../models/Course")
+const {uploadImagecloud} = require("../utils/imageUpload")
+require("dotenv").config()
+
+// create the course
+exports.createCourse = async (req,res )=>{
+    try{
+        // extract the data
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+        // get the thumbnail
+        const thumbnail = req.files.thumbnailimage;
+        // validation
+        if(!thumbnail || !courseDescription || !courseName || !whatYouWillLearn || !tag || !price){
+            return res.status(404).json({
+                success: false,
+                message:"All fields are required"
+            })
+        }
+        // check  for the instructor
+        const userid = req.user.id;
+        const instructordetails = await User.findById(userid);
+        console.log(instructordetails);
+
+        if(!instructordetails){
+            return res.status(404).json({
+                success: false,
+                message:"Instructor details not found"
+            });
+        }
+        // check if the given tag is valid or not
+        const tagdetails = await Tag.findById(tag);
+        if(tagdetails){
+            return res.status(404).json({
+                success: false,
+                message:"Tag details not found"
+            });
+        }
+        // upload image to cloudinary server
+        const thumbnailimage = await uploadImagecloud(thumbnail, process.env.FOLDER_NAME)
+
+        // create entry in the new course
+        const newCourse = await Course.create({
+            courseName, courseDescription, instructor : instructordetails._id, whatYouWillLearn,
+            price, tag : tagdetails._id, thumbnail : thumbnailimage.secure_url
+        })
+    }
+    catch(e){
+
+    }
+}
