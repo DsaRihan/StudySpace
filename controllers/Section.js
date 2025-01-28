bca314b51d77ebca25d367d5311b69e4916aa525
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-01-28 11:01:24 +0530

    added the section controller

---
+++ controllers/Section.js
@@ -0,0 +1,34 @@
+const Section = require("../models/Section")
+const Course = require("../models/Course")
+
+exports.createSection = async (req, res) => {
+    try{
+        // fetch the data
+        const {sectionName, courseId} = req.body;
+        // validate
+        if(!sectionName || !courseId){
+            return res.status(404).json({
+                success: false,
+                message:"Missing required fields"
+            })
+        }
+        // create the section
+        const newSection = await Section.create({sectionName})
+        // update the course with the new objextID
+        const updatedCourse = await Course.findByIdAndUpdate(
+            courseId,{$push:{courseContent:newSection._id}},{new:true}
+        )
+        // return response
+        return res.status(200).json({
+            success: true,
+            message:"Section created successfully",
+            updatedCourse,
+        })
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message:"Error creating Section"
+        })
+    }
+}
